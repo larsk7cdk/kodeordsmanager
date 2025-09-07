0fd732d949edcab9058babe27696035914b80460
Lars <lars@k7c.dk>
2025-09-07 12:30:20 +0200

    opdateret ui og tilføjet git workflow for ui

--- frontend/kodeordsmanager.ui/src/app/app.config.ts
+++ frontend/kodeordsmanager.ui/src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Material from '@primeuix/themes/material';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // PrimeNG
+    provideAnimationsAsync(),   // Only required if animations are needed.
+    providePrimeNG({
+      theme: {
+        preset: Material
+      }
+    })
   ]
 };
--- frontend/kodeordsmanager.ui/src/app/app.ts
+++ frontend/kodeordsmanager.ui/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {Dialog, DialogModule} from 'primeng/dialog';
+import {ButtonDirective, ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,
+    DialogModule,
+    InputTextModule,
+    ButtonModule,],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('kodeordsmanager.ui');
+  showLogin = true;       // Dialog vises i midten ved load
+  username = '';
+
+  login() {
+    // TODO: kald din auth-service her
+    console.log('Logger ind som:', this.username);
+  }
 }
